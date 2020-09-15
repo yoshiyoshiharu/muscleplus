f4e67c94cbcedd9c8896239b3576480e41a8f457
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2020-09-15 23:10:16 +0900

    常時ssl化

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,5 @@ Route::post('/comments' , 'CommentsController@store');
 Route::post('/comments/{comment}' , 'CommentsController@destroy')->where('comment', '[0-9]+');
 
 Route::get('ajax/tags/{user}', 'Ajax\TagsController@index')->where('user', '[0-9]+');
+
+Route::get('login/google', 'Auth\LoginController@redirectToGoogle')->name('login.google');
