b774b09e15e24d650f3d85cc08c2e9b3b54df1b1
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2020-09-16 16:19:30 +0900

    [revert]常時ssl化

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use App\User;
 use Socialite;
+use App\User;
+use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     /*
@@ -50,33 +51,7 @@ class LoginController extends Controller
         return back();
     }
 
-    public function redirectToGoogle()
-       {
-           return Socialite::driver('google')->redirect();
-       }
 
-  /*     public function handleGoogleCallback()
-       {
-           $gUser = Socialite::driver('google')->stateless()->user();
-           // email が合致するユーザーを取得
-           $user = User::where('email', $gUser->email)->first();
-           // 見つからなければ新しくユーザーを作成
-           if ($user == null) {
-               $user = $this->createUserByGoogle($gUser);
-           }
-           // ログイン処理
-           \Auth::login($user, true);
-           return redirect('/home');
-       }
 
-       public function createUserByGoogle($gUser)
-       {
-           $user = User::create([
-               'name'     => $gUser->name,
-               'email'    => $gUser->email,
-               'password' => \Hash::make(uniqid()),
-           ]);
-           return $user;
-       }
-       */
+
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -63,13 +63,6 @@
                             </div>
                         </div>
                     </form>
-                    <div class="form-group row mt-2">
-                        <div class="col-md-8 offset-md-4">
-                            <a href="{{route('login.google')}}" class="btn btn-secondary" role="button">
-                                Google Login
-                            </a>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
